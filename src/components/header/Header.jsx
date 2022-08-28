dd2bc015ef143ae08f475439a58b80041ba288f6
J. Ricardo Silva <38896158+JoaoRicardo-Silva@users.noreply.github.com>
2022-08-28 14:33:49 -0300

    ADD React icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header__container"></div>
+      <h5>Hello I'm</h5>
+      <h1>João Ricardo</h1>
+      <h5 className="text-light">Fullstack Developer</h5>
+      <CTA />
+      <HeaderSocials></HeaderSocials>
 
-export default Header
+      <div className="me">
+        <img src={ME} alt="" />
+      </div>
+      <a href="#contact" className="scrooll__down">
+        Scroll Sown
+      </a>
+    </header>
+  );
+};
+
+export default Header;
