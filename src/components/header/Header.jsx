70a7a56a954dc69a8d16c1a18fd7174c2d773719
JoaoRicardo-Silva <joao.ricardo.ifal@gmail.com>
2022-10-03 00:15:52 -0300

    Update header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,19 +7,21 @@ import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
     <header>
-      <div className="container header__container"></div>
+      <div className="container header__container">
       <h5>Hello I'm</h5>
       <h1>João Ricardo</h1>
-      <h5 className="text-light">Fullstack Developer</h5>
+      <h5 className="text-light">Frontend Developer</h5>
       <CTA />
       <HeaderSocials></HeaderSocials>
 
       <div className="me">
         <img src={ME} alt="" />
       </div>
-      <a href="#contact" className="scrooll__down">
+      
+      <a href="#contact" className="scroll__down">
         Scroll Sown
       </a>
+      </div>
     </header>
   );
 };
