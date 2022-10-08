d9dd5ee5eaaaef03d94b23508ae3be181bcae7a2
JoaoRicardo-Silva <joao.ricardo.ifal@gmail.com>
2022-10-08 14:27:51 -0300

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,19 +8,19 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-      <h5>Hello I'm</h5>
-      <h1>João Ricardo</h1>
-      <h5 className="text-light">Frontend Developer</h5>
-      <CTA />
-      <HeaderSocials></HeaderSocials>
+        <h5>Hello I'm</h5>
+        <h1>João Ricardo</h1>
+        <h5 className="text-light">Fullsttack Developer</h5>
+        <CTA />
+        <HeaderSocials></HeaderSocials>
 
-      <div className="me">
-        <img src={ME} alt="" />
-      </div>
-      
-      <a href="#contact" className="scroll__down">
-        Scroll Sown
-      </a>
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Sown
+        </a>
       </div>
     </header>
   );
