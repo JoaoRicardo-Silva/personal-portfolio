0dea1bbbd3847d330482dfe629299e4a1d7fcb04
JoaoRicardo-Silva <joao.ricardo.ifal@gmail.com>
2023-01-04 15:06:03 -0300

    conect test

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,7 +4,7 @@ import { BsLinkedin, BsGithub } from "react-icons/bs";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank">
+      <a href="https://www.linkedin.com/in/joaoricadosilvasantos/" target="_blank">
         <BsLinkedin />
       </a>
       <a href="https://github.com" target="_blank">
